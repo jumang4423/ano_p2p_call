d08b34eb87fa7f66c77604f6432b49d3ef3765c7
jumang4423 <catontheskate4423@gmail.com>
2022-07-17 17:05:36 +0900

    new feature or bugs, i dunno lmao

--- src/funcs/meta.ts
+++ src/funcs/meta.ts
@@ -4,5 +4,5 @@ export const systemMeta = {
   desc: "privacy first p2p call web app",
   developer: 'jumango',
   version: "0.0.1",
-  github_repository_url: "https://www.github.com/jumang4423/ano_p2p_call",
+  github_repository_url: "https://github.com/jumang4423/ano_p2p_call/tree/main"
 }
